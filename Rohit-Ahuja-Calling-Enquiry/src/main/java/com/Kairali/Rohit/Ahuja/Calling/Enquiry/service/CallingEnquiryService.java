f7478d36cae5ec59ac0ef9f99dd91b94bee3a8a0
shailesh56789 <shaishailesh00@gmail.com>
2026-01-19 18:04:07 +0530

    Add pagination support to CallingEnquiryService

--- Rohit-Ahuja-Calling-Enquiry/src/main/java/com/Kairali/Rohit/Ahuja/Calling/Enquiry/service/CallingEnquiryService.java
+++ Rohit-Ahuja-Calling-Enquiry/src/main/java/com/Kairali/Rohit/Ahuja/Calling/Enquiry/service/CallingEnquiryService.java
@@ -1,8 +1,14 @@
 package com.Kairali.Rohit.Ahuja.Calling.Enquiry.service;
 
 import com.Kairali.Rohit.Ahuja.Calling.Enquiry.domain.CallingEnquiry;
+
+import org.springframework.boot.data.autoconfigure.web.DataWebProperties;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import org.springframework.data.domain.Pageable;   // ✅
+import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
 
@@ -13,5 +19,5 @@ public interface CallingEnquiryService {
     public List<CallingEnquiry> getAllByName(String name);
     public List<CallingEnquiry> getAllByPhoneNo(String phoneNo);
     public List<CallingEnquiry> getAllByEmail(String email);
-
+   public Page<CallingEnquiry> getAll(Pageable pageable);
 }
