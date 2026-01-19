19e3e74fec4bd71a7e62ff1358e4cd78ba53d919
shailesh56789 <shaishailesh00@gmail.com>
2026-01-19 18:01:47 +0530

    Add pagination method to CallingEnquiryRepo
    Added pagination support to the CallingEnquiryRepo.

--- Rohit-Ahuja-Calling-Enquiry/src/main/java/com/Kairali/Rohit/Ahuja/Calling/Enquiry/repository/CallingEnquiryRepo.java
+++ Rohit-Ahuja-Calling-Enquiry/src/main/java/com/Kairali/Rohit/Ahuja/Calling/Enquiry/repository/CallingEnquiryRepo.java
@@ -1,12 +1,19 @@
 package com.Kairali.Rohit.Ahuja.Calling.Enquiry.repository;
 
 import com.Kairali.Rohit.Ahuja.Calling.Enquiry.domain.CallingEnquiry;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;   // ✅ CORRECT
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 
 import java.util.List;
 @Repository
 public interface CallingEnquiryRepo extends JpaRepository<CallingEnquiry,String> {
+  Page<CallingEnquiry> findAll(Pageable page);
   List<CallingEnquiry> findAllById(String id);
     List<CallingEnquiry> findByNameOfClient(String nameOfClient);
     List<CallingEnquiry> findAllByEmailId(String emailId);
